19d0650942a0e5552ba241edf826b959e9886919
Ryan Wicks <ryancwicks@gmail.com>
2018-02-21 23:56:58 -0500

    Added settings and install scripts.

---
+++ vim/installPlugins.sh
@@ -0,0 +1,7 @@
+#install pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+mkdir ~/.vim/bundle
+
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
