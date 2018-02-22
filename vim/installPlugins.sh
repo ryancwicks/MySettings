63b98319220697ff0b7551fed804279c23e43099
Ryan Wicks <ryancwicks@gmail.com>
2018-02-22 00:02:51 -0500

    added colour scheme for vim.

--- vim/installPlugins.sh
+++ vim/installPlugins.sh
@@ -5,3 +5,6 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 mkdir ~/.vim/bundle
 
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+
+mkdir ~/.vim/colors
+cp eyecandy.vim ~/.vim/colors/
