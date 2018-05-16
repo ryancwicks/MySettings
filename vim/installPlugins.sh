e6b515980cbda9020b3d2eb3066a22d2bac91530
Ryan Wicks <rwicks@2grobotics.com>
2018-05-16 16:59:58 -0400

    Update vim settings to use clang format

--- vim/installPlugins.sh
+++ vim/installPlugins.sh
@@ -5,6 +5,8 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 mkdir ~/.vim/bundle
 
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+git clone https://github.com/rhysd/vim-clang-format.git ~/.vim/bundle/vim-clang-format
 
 mkdir ~/.vim/colors
 cp eyecandy.vim ~/.vim/colors/
+cp vimrc ~/.vimrc
